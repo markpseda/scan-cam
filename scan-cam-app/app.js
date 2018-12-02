3383fd17e1d5e08a1ac1d1f356e849606fc21298
Chima Akparanta <chima@udel.edu>
2018-12-02 18:47:09 -0500

    -added simple form for uploading data to mongodb. accepts picture, license #, time, gps coords -working on page for viewing data from db. Everything work fine accept for the images

--- scan-cam-app/app.js
+++ scan-cam-app/app.js
@@ -4,26 +4,21 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser= require('body-parser');
-var MongoClient = require('mongodb').MongoClient;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dataRouter = require('./routes/data');
 
 var app = express();
-var db;
+
 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-MongoClient.connect('mongodb://scancam:camscan1@ds231229.mlab.com:31229/scan-cam-data', (err, client) => {
-    if(err){
-      console.log(err);
-    }
-    db = client.db('scan-cam-data');
-});
+
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -34,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/data',dataRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
