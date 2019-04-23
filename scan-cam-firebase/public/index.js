bdf4821434174406a7a6ecf2d9297ec4b09ed875
Mark Seda <mseda@udel.edu>
2019-04-23 14:36:34 -0400

    Working stable now for gps coords with spaces periods etc. Next step is url uploading...

--- scan-cam-firebase/public/index.js
+++ scan-cam-firebase/public/index.js
@@ -6,56 +6,8 @@ firestore.settings(settings);
 // Create a storage reference from our storage service
 var storageRef = storage.ref();
 
-
 // Create reference to uploads collection
 var uploadsRef = firestore.collection('uploads');
-
-
-// Update table once on page load
-uploadsRef.get().then(function(uploads) {
-  console.log("Should only happen once");
-  $("#table-of-uploads").empty();
-    uploads.forEach(function(upload){
-        var uploadData = upload.data();
-
-        var date = new Date(uploadData.timestamp);
-
-        var imgRef = storageRef.child(uploadData.imageRef + '.jpg');
-
-        var imgURL = null;
-
-        $("#table-of-uploads").append('<tr><td id = "' + upload.id +'">' + uploadData.license_number + '</td><td>' + uploadData.gps_coords + '</td><td>' + date + '</td><td> <img id="' + uploadData.imageRef + '" src="' + imgURL + '" height = "100" width = "200"/></td>');
-
-        imgRef.getDownloadURL().then(function(url)
-        {
-            imgURL = url;
-            console.log("Found image in storage");
-            console.log(uploadData.imageRef);
-            console.log(imgURL);
-            $('#' + uploadData.imageRef).attr("src", imgURL);
-        }).catch(function(error) {
-            switch (error.code) {
-              case 'storage/object-not-found':
-                // File doesn't exist
-                break;
-          
-              case 'storage/unauthorized':
-                // User doesn't have permission to access the object
-                break;
-          
-              case 'storage/canceled':
-                // User canceled the upload
-                break;
-              case 'storage/unknown':
-                // Unknown error occurred, inspect the server response
-                break;
-            }
-        });
-    });
-});
-
-
-
 
 $("#upload-data").click(function (event) {
     console.log("Made it here!");
@@ -68,12 +20,9 @@ $("#upload-data").click(function (event) {
     console.log("GPS Coords: " + gpsCoords)
     console.log("Image: " + licensePlateImage)
 
-
     var timestamp = Date.now();
 
     var imageRef = licensePlateNum + gpsCoords
-
-    // TODO: Upload file first, get a reference to add to firestore
 
     firestore.collection("uploads").add({
         timestamp : timestamp,
@@ -82,39 +31,40 @@ $("#upload-data").click(function (event) {
         imageRef : imageRef
 
     });
-
     imageReff = storageRef.child(imageRef + '.jpg');
-
     imageReff.put(licensePlateImage).then(function(snapshot){
-        console.log("Did a thing!");
+        // the initial load image will fail because of asyncronous-ness. Add the source link after the file is added to storage
+        imageReff.getDownloadURL().then(function(url)
+        {
+          $('#' + imageRef.split(' ').join('').split(',').join('').split('.').join('')).attr("src", url);
+        });
+        
     });
-
-
-
-
 });
-
 
 uploadsRef.orderBy('timestamp').onSnapshot(function(uploads){
     $("#table-of-uploads").empty();
     uploads.forEach(function(upload){
-        console.log("Should only happen once per upload.");
         var uploadData = upload.data();
 
         var date = new Date(uploadData.timestamp);
 
         var imgRef = storageRef.child(uploadData.imageRef + '.jpg');
 
-        var imgURL = null;
+        var imgURL = "";
 
-        $("#table-of-uploads").append('<tr><td id = "' + upload.id +'">' + uploadData.license_number + '</td><td>' + uploadData.gps_coords + '</td><td>' + date + '</td><td> <img id="' + uploadData.imageRef + '" src="' + imgURL + '" height = "100" width = "200"/></td>');
+        $("#table-of-uploads").append('<tr><td id = "' + upload.id +'">' + uploadData.license_number + '</td><td>' + uploadData.gps_coords + '</td><td>' + date + '</td><td> <img id="' + uploadData.imageRef.split(' ').join('').split(',').join('').split('.').join('') + '" src="' + imgURL + '" height = "100" width = "200"/></td>');
 
         imgRef.getDownloadURL().then(function(url)
         {
             imgURL = url;
             console.log("Found image in storage");
-            $('#' + uploadData.imageRef).attr("src", imgURL);
+            console.log(uploadData.imageRef.split(' ').join(''));
+            console.log(imgURL);
+
+            $('#' + uploadData.imageRef.split(' ').join('').split(',').join('').split('.').join('')).attr("src", imgURL);
         }).catch(function(error) {
+          console.log("Some kinda storage error!");
             switch (error.code) {
               case 'storage/object-not-found':
                 // File doesn't exist
@@ -132,7 +82,5 @@ uploadsRef.orderBy('timestamp').onSnapshot(function(uploads){
                 break;
             }
         });
-
-
     });
 });
