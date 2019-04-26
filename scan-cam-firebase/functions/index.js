30746561ab0253e6be9d4ed24c76537ffd224871
Mark Seda <mseda@udel.edu>
2019-04-26 11:50:43 -0400

    added url to piGUI, in process on file uploads

--- scan-cam-firebase/functions/index.js
+++ scan-cam-firebase/functions/index.js
@@ -2,6 +2,9 @@ const functions = require('firebase-functions');
 
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
+
+var fireupload = require("express-fileupload")
+
 admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
@@ -9,11 +12,40 @@ admin.initializeApp();
 
 // Take the text parameter passed to this HTTP endpoint and insert it into the
 // Realtime Database under the path /messages/:pushId/original
-exports.addMessage = functions.https.onRequest(async (req, res) => {
-    // Grab the text parameter.
-    const original = req.query.text;
-    // Push the new message into the Realtime Database using the Firebase Admin SDK.
-    const snapshot = await admin.firestore().collection('messages').doc('message1').set({text:original});
-    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-    res.redirect(303, snapshot.ref.toString());
+exports.uploadData = functions.https.onRequest(async (req, res) => {
+
+    if (req.method != "POST")
+    {
+        res.status(400).send("Should be a POST request at this URL.");
+        return
+    }
+
+
+    // Grab the text parameters.
+    const timestamp = req.query.timestamp;
+    const license_number = req.query.license_number;
+    const gps_coords = req.query.gps_coords;
+    const imageRef = license_number + gps_coords;
+
+    file = req.files;
+    console.log(file);
+    console.log("this is a test")
+    // upload file to storage first, then update the database
+
+    //await functions.storage.upload(file, {destination:imageRef + '.jpg'}).then(() => {
+        // Push the new data into firestore.
+        
+   // });
+
+    const snapshot = await admin.firestore().collection('uploads').add({
+        timestamp : timestamp,
+        license_number: license_number,
+        gps_coords : gps_coords,
+        imageRef : imageRef
+    });
+
+    // Respond with sucess...
+    res.status(201).send("Successfully added new item to database.");
+
+
 });
