7b1ef9362ee503058c0685d4efb2dfa1f4216a11
Mark Seda <mseda@udel.edu>
2019-04-28 23:01:50 -0400

    Basic upload of document and the image working! Should just need to add this to main.py along with the json account key

--- python_fb.py
+++ python_fb.py
@@ -3,21 +3,34 @@ from google.cloud import storage
 
 import google.oauth2.credentials
 
+# Really bad practice here, but this lets us access the services we need
 storage_client = storage.Client.from_service_account_json('service_account.json')
-
-    
 db = firestore.Client.from_service_account_json('service_account.json')
+
+
+
+
+
 
 doc_ref = db.collection(u'uploads')
 
 new_doc = doc_ref.document()
 
-timestamp = '12345'
+timestamp = '3425125'
 gps_coords = '23.2, 32.1'
-license_number = '2342A4'
+license_number = '233422A4'
 imageRef = license_number + gps_coords
 
 
+# just the default top level bucket
+bucket = storage_client.get_bucket('ud-senior-design-2018-scan-cam.appspot.com')
+
+blob = bucket.blob(imageRef + '.jpg')
+
+blob.upload_from_filename('LTM378.jpg')
+
+
+# This uploads the document
 new_doc.set({
     u'timestamp' : timestamp,
     u'gps_coords' : gps_coords,
@@ -25,6 +38,3 @@ new_doc.set({
     u'license_number' : license_number
 })
 
-buckets = list(storage_client.list_buckets())
-
-print(buckets)
